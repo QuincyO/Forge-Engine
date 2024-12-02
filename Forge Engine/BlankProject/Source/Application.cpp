16944ddb55a987cddab4fd5f29be68690b247a8d
QuincyO <quincyorta@gmail.com>
2024-12-02 02:23:18 -0500

    Add new source and header files to projects
    Updated `BlankProject.vcxproj` and `BlankProject.vcxproj.filters` to include new source (`Application.cpp`, `pch.cpp`, `WinMain.cpp`) and header files (`Application.h`, `pch.h`). Organized files under `Source\Private` and `Source\Public`.
    
    Updated `Forge.vcxproj` and `Forge.vcxproj.filters` to include new source (`IApplication.cpp`) and header files (`IApplication.h`, `WinEntry.h`) under `Source\Platform\WIN32`. Modified preprocessor definitions and subsystem settings. Updated `AdditionalIncludeDirectories`.
    
    Included `Forge.h` in `Forge.cpp`. Created new `Forge.h` with `#pragma once` and conditional `Windows.h` inclusion.
    
    Created `Application.cpp` and `Application.h` with `Application` class definition, including constructor, destructor, `Initialize`, and `Update` methods.

---
+++ Forge Engine/BlankProject/Source/Application.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "Application.h"
+
+Application::Application()
+{
+
+}
+
+Application::~Application()
+{
+
+}
+
+VOID Application::Initialize()
+{
+	return VOID();
+}
+
+VOID Application::Update()
+{
+	return VOID();
+}
---
+++ Forge Engine/BlankProject/Source/Application.h
@@ -0,0 +1,36 @@
+#pragma	once
+
+class Application {
+	
+
+
+	//My Application
+
+public:
+	
+	//Application Constructor
+	
+	
+	Application();
+	
+	
+	
+	//Application Destructor
+	
+	
+	~Application();
+
+
+public:
+
+	// Called to Initialize the Application
+
+	VOID Initialize();
+
+	/* Game Loop - Called on a loop while the application is running */
+
+	VOID Update();
+
+
+
+};
